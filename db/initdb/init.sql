257bba441560e77b02b05b76712eac6165029ad1
Chris Sheridan <csheri46@gmail.com>
2025-11-07 14:11:52 -0500

    Added remaining CRUD functions (GET, PUT, and DELETE)

--- db/initdb/init.sql
+++ db/initdb/init.sql
@@ -2,4 +2,4 @@ CREATE TABLE angler ( angler_id SERIAL PRIMARY KEY, email_addr text NOT NULL, na
 
 CREATE TABLE trip ( trip_id SERIAL PRIMARY KEY, angler_id INT REFERENCES angler(angler_id) ON DELETE CASCADE, num_anglers int, trip_date date, area_fished text, bait_type text, fishing_type text, fishing_time interval, target_trout boolean, trout_time interval, target_bass boolean, bass_time interval, target_pike boolean, pike_time interval, target_yp boolean, yp_time interval, target_wp boolean, wp_time interval, target_sunfish boolean, sunfish_time interval, target_bullhead boolean, bullhead_time interval, no_fish boolean, personal_notes text );
 
-CREATE TABLE fish (trip_id INT REFERENCES trip(trip_id) ON DELETE CASCADE, species text, length text, kept boolean, released boolean);
+CREATE TABLE fish (fish_id SERIAL PRIMARY KEY, trip_id INT REFERENCES trip(trip_id) ON DELETE CASCADE, species text, length text, kept boolean, released boolean);
--- server/server.js
+++ server/server.js
@@ -70,3 +70,107 @@ app.post("/api", async (req, res) => {
         client.release();
     }
 });
+
+app.get("/api/angler", async (req, res) => {
+    try{
+        const result = await pool.query(
+            'SELECT * FROM angler',
+        )
+        res.status(200).json(result.rows);
+    }
+    catch (err){
+        console.error(err);
+        res.status(500).json({error: 'Error retrieving data'});
+    }
+
+})
+
+
+app.get("/api/trip", async (req, res) => {
+    try{
+        const result = await pool.query(
+            'SELECT * FROM trip',
+        )
+        res.status(200).json(result.rows);
+    }
+    catch (err){
+        console.error(err);
+        res.status(500).json({error: 'Error retrieving data'});
+    }
+})
+
+app.get("/api/fish", async (req, res) => {
+    try{
+        const result = await pool.query(
+            'SELECT * FROM fish',
+        )
+        res.status(200).json(result.rows);
+    }
+    catch (err){
+        console.error(err);
+        res.status(500).json({error: 'Error retrieving data'});
+    }
+})
+
+app.put("/api/angler/:angler_id", async(req, res) => {
+    const id = req.params.angler_id;
+    const{email_addr, name_first, name_last} = req.body;    
+    try{
+        const result = await pool.query(
+            'UPDATE angler SET email_addr = $2, name_first = $3, name_last = $4 WHERE angler_id =$1 RETURNING *',
+            [id, email_addr, name_first, name_last]
+        )
+        res.status(200).json(result.rows[0]);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({error: 'Error updating data'})
+    }
+})
+
+app.put("/api/trip/:trip_id", async(req, res) => {
+    const id = req.params.trip_id;
+    const {num_anglers, trip_date, area_fished, bait_type, fishing_type, fishing_time, target_trout, trout_time, target_bass, bass_time, target_pike, pike_time, target_yp, yp_time, target_wp, wp_time, target_sunfish, sunfish_time, target_bullhead, bullhead_time, no_fish, personal_notes} = req.body;
+    try{
+        const result = await pool.query(
+            'UPDATE trip SET num_anglers = $2, trip_date = $3, area_fished = $4, bait_type = $5, fishing_type = $6, fishing_time = $7, target_trout = $8, trout_time = $9, target_bass = $10, bass_time = $11, target_pike = $12, pike_time = $13, target_yp = $14, yp_time = $15, target_wp = $16, wp_time = $17, target_sunfish = $18, sunfish_time = $19, target_bullhead = $20, bullhead_time = $21, no_fish = $22, personal_notes = $23 WHERE trip_id =$1 RETURNING *',
+            [id, num_anglers, trip_date, area_fished, bait_type, fishing_type, fishing_time, target_trout, trout_time, target_bass, bass_time, target_pike, pike_time, target_yp, yp_time, target_wp, wp_time, target_sunfish, sunfish_time, target_bullhead, bullhead_time, no_fish, personal_notes]
+        )
+        res.status(200).json(result.rows[0]);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({error: 'Error updating data'})
+    }
+})
+
+app.put("/api/fish/:fish_id", async(req, res) => {
+    const id = req.params.fish_id;
+    const {species, length, kept, released} = req.body;
+    try{
+        const result = await pool.query(
+            'UPDATE fish SET species = $2, length=$3, kept=$4, released=$5 WHERE fish_id =$1 RETURNING *',
+            [id, species, length, kept, released]
+        )
+        res.status(200).json(result.rows[0]);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({error: 'Error updating data'})
+    }
+})
+
+app.delete("/api/angler/:angler_id", async(req, res) => {
+    const id = req.params.angler_id;
+    try{
+        const result = await pool.query(
+            'DELETE FROM angler WHERE angler_id = $1',
+            [id]
+        )
+        res.status(200).json();
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({error: 'Error removing data'})
+    }
+})
